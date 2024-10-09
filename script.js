084cafdef7621eb5b04e53207b50af7c34327746
WebMohit20 <2003mohitkhemani@gmail.com>
2024-10-10 00:44:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let date = new Date()
+let timer = document.getElementById("timer");
+let mdy = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+let time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+timer.innerHTML = `${mdy}, ${time}`
